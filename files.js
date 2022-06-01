d44a5eb3df3d0e10aff2c8fdffe8934d031c2ada
pawelworzala <103989025+pawelworzala@users.noreply.github.com>
2022-06-01 21:59:49 +0200

    ref

---
+++ files.js
@@ -0,0 +1,54 @@
+const files=[
+
+    "/scripts/node.js"
+
+    ,"/scripts/red/init.js"
+
+    ,"/scripts/red/factories/compile.js"
+    //,"/scripts/red/factories/http.js"
+    ,"/scripts/red/factories/watch.js"
+    //,"/scripts/red/factories/window.js"
+
+    ,"/scripts/red/directives/click.js"
+
+  
+    ,"/scripts/webgl/common.js"
+    ,"/scripts/webgl/camera.js"
+    ,"/scripts/webgl/scene.js"
+
+    
+    ,"/scripts/webgl/buffer.js"
+
+    ,"/scripts/webgl/engine.js"
+
+    ,"/scripts/webgl/geometry.js"
+    ,"/scripts/webgl/mat4.js"
+    ,"/scripts/webgl/mesh.js"
+    ,"/scripts/webgl/shader.js"
+    ,"/scripts/webgl/texture.js"
+    ,"/scripts/webgl/uniform.js"
+    ,"/scripts/webgl/vec3.js"
+
+    ,"/scripts/webgl/render2d.js"
+
+    ,"/scripts/webgl/gltf/model.js"
+    ,"/scripts/webgl/gltf/anim.js"
+    ,"/scripts/webgl/gltf/skin.js"
+
+  
+    ,"/scripts/webgl/scene/cubes.js"
+    ,"/scripts/webgl/scene/triangulation.js"
+    ,"/scripts/webgl/scene/render2d.js"
+    ,"/scripts/webgl/scene/modelGLTF.js"
+
+
+
+
+
+  
+    ,"/scripts/index.js"
+]
+
+try{
+    module.exports=files
+}catch(e){}
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
-app.use('/shaders',express.static('shaders'))
-app.use('/scripts',express.static('scripts'))
-app.use(express.static('dist'))
+app.use(express.static('./'))
+//app.use('/shaders',express.static('shaders'))
+//app.use('/scripts',express.static('scripts'))
+//app.use(express.static('dist'))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve('./public/index.html'))
+  res.sendFile(path.resolve('./index.html'))
 })
 
 app.listen(3000)
